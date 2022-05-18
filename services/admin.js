e5b7422c1814c1d9a05c7ff7f58744fdde2441f8
Diego <A00827847@itesm.mx>
2022-05-18 18:15:05 -0500

    Ordered users by name

--- services/admin.js
+++ services/admin.js
@@ -3,7 +3,7 @@ const timestamp = new Date();
 
 module.exports = { 
     getAllEmployees : () => {
-    sql = 'SELECT employee_id, username, employee_name, employee_last_name, department_id, is_admin FROM employee'
+    sql = 'SELECT employee_id, username, employee_name, employee_last_name, department_id, is_admin FROM employee ORDER BY employee_name ASC'
     return new Promise( (resolve, reject) => {
       pool.query(sql, (err, res) => {
         if(err) { 
