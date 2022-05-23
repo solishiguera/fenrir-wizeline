5e0360504adbcc0d68a10a9ae1379e973e7ddb30
Diego <A00827847@itesm.mx>
2022-05-23 11:00:18 -0500

    initial sequelize

--- config/db.js
+++ config/db.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: { // Para correr en dev, se debe comentar esto
-        rejectUnauthorized: false,
-    },
-});
-module.exports = pool;
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('postgres://diego:diego@localhost:5432/fenrirwizeline') // Example for postgres
+
+module.exports = sequelize;
