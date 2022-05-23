50b3b0956a2709b8124bf411bebf6aa36584eb94
Diego <A00827847@itesm.mx>
2022-05-23 14:07:31 -0500

    Added question model

---
+++ model/question.js
@@ -0,0 +1,64 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+module.exports = {
+  Question : sequelize.define('question', {
+    question_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    }, 
+
+    employee_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    department_id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false
+    },
+
+    question_text: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+
+    is_anonymous: { 
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }, 
+
+    date_created: {
+      type: DataTypes.timestamps
+    },
+
+    date_last_modified: {
+      type: DataTypes.timestamps
+    }, 
+
+    like_count: { 
+      type: DataTypes.INTEGER
+    },
+
+    comment_count: {
+      type: DataTypes.INTEGER
+    },
+
+    is_answered: {
+      type: DataTypes.BOOLEAN
+    },
+
+    ask_employee_id: {
+      type: DataTypes.INTEGER
+    }
+  }, 
+  {
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
+    freezeTableName: true
+  })
+
+
+}
