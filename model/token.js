9e8666d60ae8b2e00b0514130f12413da6646792
AnaVea19 <77868953+AnaVea19@users.noreply.github.com>
2022-05-24 17:11:05 -0500

    Model
    Add comment, department, likes and token

---
+++ model/token.js
@@ -0,0 +1,37 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+module.exports = { 
+  Token : sequelize.define('token', {
+        token_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+        }, 
+
+        username: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+
+        token: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+
+        date_created: { 
+        type : DataTypes.DATE
+        },
+
+        expiration_date: { 
+        type : DataTypes.DATE
+        },
+    }, 
+    {
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false,
+        freezeTableName: true
+    })
+}
