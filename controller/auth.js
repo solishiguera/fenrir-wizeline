be48695d8f20e5c9a5194f06cac44f1646e60d62
Diego <A00827847@itesm.mx>
2022-05-24 15:26:19 -0500

    medio funciona

--- controller/auth.js
+++ controller/auth.js
@@ -7,21 +7,20 @@ module.exports = {
     try {
       
       const employee = await AuthServices.login(req.body.username);
-
       // Si no existe usuario
       if(Object.keys(employee).length === 0) { 
         throw 'Usuario no existe';
       }
 
       // Validate password
-      const correctPassword = await SecureEnv.comparePassword(req.body.employee_password, employee[0]['employee_password'])
+      const correctPassword = await SecureEnv.comparePassword(req.body.employee_password, employee['employee_password'])
       if(!correctPassword) { 
         throw 'Contraseña incorrecta';
       }
 
       const accessToken = SecureEnv.generateAccessToken(employee)
       const refreshToken = SecureEnv.generateRefreshToken(employee)
-      const save = AuthServices.saveRefreshToken(refreshToken, employee[0]['username'])
+      const save = AuthServices.saveRefreshToken(refreshToken, employee['username'])
 
       res.json({accessToken, refreshToken})
     } catch (err) {
--- services/auth.js
+++ services/auth.js
@@ -1,23 +1,22 @@
-const pool = require('../config/db');
+const poolQ = require('../config/db');
 const timestamp = new Date();
+const Model = require('../model/employee');
 
 module.exports = { 
-  login : (username) => {
-    sql = "SELECT * from employee WHERE username = $1"
-    return new Promise( (resolve, reject) => {
-      pool.query(sql,[username], (err, res) => {
-        if(err) { 
-          return reject(err)
-        }
-        return resolve(res.rows)
-      })
-    })
+  login : async (username) => {
+    try {
+      const employee = await Model.Employee.findOne({ where: { username: username } })
+      return employee;
+    } catch (error) {
+      console.log(`Error al obtener username: Error: ${error}`)
+    }
+    
   },
 
   saveRefreshToken : (token, username) => { 
     sql = "INSERT INTO token(username, token, date_created, expiration_date) VALUES($1, $2, $3, $4);"
     return new Promise( (resolve, reject) => {
-      pool.query(sql,[username, token, timestamp, null], (err, res) => {
+      poolQ.query(sql,[username, token, timestamp, null], (err, res) => {
         if(err) { 
           return reject(err)
         }
@@ -29,7 +28,7 @@ module.exports = {
   deleteRefreshToken : (token) => {
     sql = "DELETE FROM token WHERE token = $1;"
     return new Promise( (resolve, reject) => {
-      pool.query(sql,[token], (err, res) => {
+      poolQ.query(sql,[token], (err, res) => {
         if(err) { 
           return reject(err)
         }
@@ -45,7 +44,7 @@ module.exports = {
 
     sql = 'INSERT INTO employee(employee_name, employee_last_name, department_id, is_admin, job_title, profile_picture, username, employee_password) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *'
     return new Promise( (resolve, reject) => {
-      pool.query(sql,[employeeName, employeeLastName, deptId, "false", jobTitle, null, username, employeePassword], (err, res) => {
+      poolQ.query(sql,[employeeName, employeeLastName, deptId, "false", jobTitle, null, username, employeePassword], (err, res) => {
         if(err) { 
           return reject(err)
         }
