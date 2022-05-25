98480e877a4c1d8dc9e71241b6288b2fbf4790d4
Diego <A00827847@itesm.mx>
2022-05-25 09:24:17 -0500

    Removed garbage

--- config/db2.js
+++ config/db2.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
 const connection = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const sequelize = new Sequelize(connection)
 
