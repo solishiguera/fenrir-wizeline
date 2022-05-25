b88df2a2c4d0aee391f3819f632fa737b63ea64c
Diego <A00827847@itesm.mx>
2022-05-24 19:42:42 -0500

    comment is done

--- controller/comment.js
+++ controller/comment.js
@@ -22,8 +22,8 @@ module.exports = {
 
     getCommentWithId : async (req, res, next) => {
         try{
-            const comments = await UserServices.getCommentWithId(req.params.id);
-            res.json({comments});
+            const comment = await UserServices.getCommentWithId(req.params.id);
+            res.json({comment});
         }catch(err){
             res.json({message:`Error al obtener comentario con ID. Err: ${err}`})
         }
