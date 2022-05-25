757ccc60bf415f7ac4749ed98288888ccd196c1c
Diego <A00827847@itesm.mx>
2022-05-24 19:55:31 -0500

    Employee is done

--- controller/employee.js
+++ controller/employee.js
@@ -2,33 +2,9 @@ const UserServices = require("../services/employee");
 const SecureEnv = require("../config/security/security");
 
 module.exports = {
-  /**
-  * @deprecated Will be deleted soon. Use login instead.
+  /** 
+   * @duplicate ?
   */
-  userSignin: async (req, res, next) => {
-    try {
-      const employee = await UserServices.userSignin(req.body.username);
-      res.json({ employee });
-    } catch (err) {
-      res.json({ message: `Error al obtener empleado. Err: ${err}` });
-    }
-  },
-
-
-  /**
-  * @deprecated Will be deleted soon. Use signup instead.
-  */
-  addEmployee: async (req, res, next) => {
-    try {
-      const securePsw = await SecureEnv.securePassword(req.body.employee_password);
-      const employee = await UserServices.addEmployee(req.body.employee_name, req.body.employee_last_name, req.body.department_id, req.body.job_title, req.body.username, securePsw);
-      const accessToken = SecureEnv.generateAccessToken(employee)
-      res.json({accessToken})
-    } catch (err) {
-      res.json({ message: `Error al agregar empleado. Err: ${err}` });
-    }
-  },
-
   updateEmployee: async (req, res, next) => {
     try {
       const employee = await UserServices.updateEmployee(req.params.id, req.body.is_admin);
