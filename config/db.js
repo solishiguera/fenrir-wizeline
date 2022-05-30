3ed6664b6b54615ef7143bb0c951d5eaa79c71df
Diego <A00827847@itesm.mx>
2022-05-30 09:51:18 -0500

    Sequelize works with AWS

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-//const connection = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-const connection = `postgres://ls-f983eab63bdedab98e6d497c1ae8716a2dadaa12.chmqwurvd2xb.us-east-1.rds.amazonaws.com:5432/dbmasteruser`
-const sequelize = new Sequelize(connection)
+/*const connectionDev = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const sequelize = new Sequelize(connectionDev)*/
+
+const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.AWS_USER, process.env.AWS_PASSWORD, {
+  host: process.env.AWS_HOST,
+  dialect: 'postgres'
+});
 
 module.exports = sequelize;
