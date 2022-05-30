4cc89b4b52276f2c5e53f34489b805301407ac95
Diego <A00827847@itesm.mx>
2022-05-29 20:09:49 -0500

    aws endpoint

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-const connection = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+//const connection = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const connection = `postgres://ls-f983eab63bdedab98e6d497c1ae8716a2dadaa12.chmqwurvd2xb.us-east-1.rds.amazonaws.com:5432/dbmasteruser`
 const sequelize = new Sequelize(connection)
 
 module.exports = sequelize;
