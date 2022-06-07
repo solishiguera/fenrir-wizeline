b139c4b7e8c57ca231ea693c1cb8d2cf8c400966
Diego <A00827847@itesm.mx>
2022-06-07 07:57:07 -0500

    db config

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-const connectionDev = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-const sequelize = new Sequelize(connectionDev)
+// DEV
+// const connectionDev = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+// const sequelize = new Sequelize(connectionDev)
 
-// const sequelize = new Sequelize(process.env.AWS_DATABASE, process.env.AWS_USER, process.env.AWS_PASSWORD, {
-//   host: process.env.AWS_HOST,
-//   dialect: 'postgres'
-// });
+const sequelize = new Sequelize(process.env.AWS_DATABASE, process.env.AWS_USER, process.env.AWS_PASSWORD, {
+  host: process.env.AWS_HOST,
+  dialect: 'postgres'
+});
 
 module.exports = sequelize;
